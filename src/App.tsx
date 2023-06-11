9a81e3b2668ad798831b3d73de4fbf496af6d2f1
Henrio Randriasoa <rioxdev@gmail.com>
2023-06-11 23:55:26 +0200

    stage 4 - wip

--- src/App.tsx
+++ src/App.tsx
@@ -6,20 +6,18 @@ import './App.css';
 import Stage1 from './stage1';
 import Stage2 from './stage2';
 import Stage3 from './stage3';
+import Stage4 from './stage4';
 
 function App() {
 
-
   return (
     <>
-
       <div className="App">
-        {/* <Stage1/> */}
-        {/* <Stage2/> */}
-        <Stage3 />
+
+        <Stage4 />
+
       </div>
     </>
-
   );
 }
 
---
+++ src/stage4.tsx
@@ -0,0 +1,18 @@
+
+import { useEffect } from "react"
+
+export default function Stage4(){
+
+    useEffect(()=> {
+        console.log('STAGE 4')
+
+
+    },[])
+
+    return(
+        <div>
+            <h4>Stage 4</h4>
+        </div>
+    )
+}
+

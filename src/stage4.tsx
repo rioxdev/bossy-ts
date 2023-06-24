4bde93c3865a9a187e9132c1c3cd02db132fc5cb
Henrio Randriasoa <rioxdev@gmail.com>
2023-06-24 14:05:06 +0200

    stage 4 - wip

--- src/stage4.tsx
+++ src/stage4.tsx
@@ -1,15 +1,38 @@
 
 import { useEffect } from "react"
 
-export default function Stage4(){
+let run1 = () => {
+    console.log('run 1')
 
-    useEffect(()=> {
+    let hat = { name: 'Hat', price: 100 }
+    let gloves = { name: 'Gloves', price: 75 }
+    let umbrella = { name: 'Umbrella', price: 23, waterproof: true }
+
+    let products: { name: string, price: number }[] = [hat, gloves, umbrella]
+    products.forEach(prod => console.log(`${prod.name}: ${prod.price}`))
+}
+
+let run2 =() => {
+    console.log('run 2')
+
+    let hat = { name: 'Hat', price: 100 }
+    let gloves = { name: 'Gloves', price: 75 }
+    let umbrella = { name: 'Umbrella', price: 23, waterproof: true }
+
+    let products: { name: string, price?: number, waterproof?: boolean }[] = [hat, gloves, umbrella]
+    products.forEach(prod => console.log(`${prod.name}: ${prod.price}`))
+}
+
+export default function Stage4() {
+
+    useEffect(() => {
         console.log('STAGE 4')
+        run1()
+        run2()
 
+    }, [])
 
-    },[])
-
-    return(
+    return (
         <div>
             <h4>Stage 4</h4>
         </div>
